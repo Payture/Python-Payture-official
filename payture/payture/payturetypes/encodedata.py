22a61dd8de2bbe316e8888d4fb3c4f2944eba614
Nikita Popovsky <nikita@popovsky.pro>
2020-06-01 13:22:55 +0300

    fix ewallet init and handling unsuccessful response

--- payture/payture/payturetypes/encodedata.py
+++ payture/payture/payturetypes/encodedata.py
@@ -39,7 +39,7 @@ class Card(EncodeString):
 
 
 class Customer(EncodeString):
-    def __init__(self, login, password, phone=None, email=None):
+    def __init__(self, login, password, phone='', email=''):
         self.VWUserLgn = login
         self.VWUserPsw = password
         self.PhoneNumber = phone
@@ -50,7 +50,7 @@ class Data(EncodeString):
     def __init__(self, sessionType, ip, **kwargs):
         self.SessionType = sessionType
         self.IP = ip
-        for key, value in kwargs.items():
+        for key, value in kwargs['kwargs'].items():
             if(key == 'OrderId'):
                 self.OrderId = value
             elif(key == 'Amount'):
